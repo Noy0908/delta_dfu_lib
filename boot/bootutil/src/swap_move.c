68f1d1ec4cad596b4b50c112157a029c0f82dc56
Noy Luo <noy.luo@nordicsemi.no>
2024-03-19 10:17:52 +0800

    delta dfu test on nRF54L15 success

--- boot/bootutil/src/swap_move.c
+++ boot/bootutil/src/swap_move.c
@@ -28,6 +28,20 @@
 
 #include "mcuboot_config/mcuboot_config.h"
 
+
+#ifdef MCUBOOT_DELTA_UPGRADE
+#include "../../zephyr/delta_dfu_lib/include/delta.h"
+#define FLASH_NODEID 	DT_CHOSEN(zephyr_flash_controller)
+extern uint32_t patch_size; 
+extern const struct device *flash_device;
+extern  uint8_t opFlag ;
+extern off_t status_address;
+extern struct detools_apply_patch_t apply_patch;
+/** variable used to indicate source image should be moved up how many pages before aplly */
+extern uint8_t move_up_pages;	
+extern fih_int get_source_hash(const struct flash_area *fap,uint8_t *hash_buf);
+#endif
+
 BOOT_LOG_MODULE_DECLARE(mcuboot);
 
 #ifdef MCUBOOT_SWAP_USING_MOVE
@@ -237,7 +251,7 @@ boot_status_internal_off(const struct boot_status *bs, int elem_sz)
 int
 boot_slots_compatible(struct boot_loader_state *state)
 {
-#ifdef PM_S1_ADDRESS
+#if defined (PM_S1_ADDRESS) || defined (MCUBOOT_DELTA_UPGRADE)
     /* Patch needed for NCS. In this case, image 1 primary points to the other
      * B1 slot (ie S0 or S1), and image 0 primary points to the app.
      * With this configuration, image 0 and image 1 share the secondary slot.
@@ -259,7 +273,7 @@ boot_slots_compatible(struct boot_loader_state *state)
     num_sectors_sec = boot_img_num_sectors(state, BOOT_SECONDARY_SLOT);
     if ((num_sectors_pri != num_sectors_sec) &&
             (num_sectors_pri != (num_sectors_sec + 1))) {
-        BOOT_LOG_WRN("Cannot upgrade: not a compatible amount of sectors");
+        BOOT_LOG_WRN("Cannot upgrade: not a compatible amount of sectors[%d:%d]",num_sectors_pri,num_sectors_sec);
         return 0;
     }
 
@@ -340,6 +354,137 @@ swap_status_source(struct boot_loader_state *state)
     return BOOT_STATUS_SOURCE_NONE;
 }
 
+
+#ifdef MCUBOOT_DELTA_UPGRADE
+static void
+boot_move_sector_up_pages(int idx, uint32_t sz, uint8_t pages, struct boot_loader_state *state,
+        struct boot_status *bs, const struct flash_area *fap_pri,
+        const struct flash_area *fap_sec)
+{
+    uint32_t new_off;
+    uint32_t old_off;
+    int rc;
+
+    /*
+     * FIXME: assuming sectors of size == sz, a single off variable
+     * would be enough
+     */
+
+    /* Calculate offset from start of image area. */
+    new_off = boot_img_sector_off(state, BOOT_PRIMARY_SLOT, idx + pages -1);
+    old_off = boot_img_sector_off(state, BOOT_PRIMARY_SLOT, idx - 1);
+
+    if (bs->idx == BOOT_STATUS_IDX_0) {
+        if (bs->source != BOOT_STATUS_SOURCE_PRIMARY_SLOT) {
+            rc = swap_erase_trailer_sectors(state, fap_pri);
+            assert(rc == 0);
+
+            rc = swap_status_init(state, fap_pri, bs);
+            assert(rc == 0);
+        }
+
+        rc = swap_erase_trailer_sectors(state, fap_sec);
+        assert(rc == 0);
+    }
+
+    rc = boot_erase_region(fap_pri, new_off, sz);
+    assert(rc == 0);
+
+    rc = boot_copy_region(state, fap_pri, fap_pri, old_off, new_off, sz);
+    assert(rc == 0);
+
+    rc = boot_write_status(state, bs);
+
+    bs->idx++;
+    BOOT_STATUS_ASSERT(rc == 0);
+}
+
+
+static void
+boot_move_sector_down_pages(int idx, uint32_t sz, uint8_t pages, struct boot_loader_state *state,
+        struct boot_status *bs, const struct flash_area *fap_pri,
+        const struct flash_area *fap_sec)
+{
+    uint32_t new_off;
+    uint32_t old_off;
+    int rc;
+
+    /*
+     * FIXME: assuming sectors of size == sz, a single off variable
+     * would be enough
+     */
+
+    /* Calculate offset from start of image area. */
+    old_off = boot_img_sector_off(state, BOOT_PRIMARY_SLOT, idx + pages -1);
+    new_off = boot_img_sector_off(state, BOOT_PRIMARY_SLOT, idx - 1);
+
+    if (bs->idx == BOOT_STATUS_IDX_0) {
+        if (bs->source != BOOT_STATUS_SOURCE_PRIMARY_SLOT) {
+            rc = swap_erase_trailer_sectors(state, fap_pri);
+            assert(rc == 0);
+
+            rc = swap_status_init(state, fap_pri, bs);
+            assert(rc == 0);
+        }
+
+        rc = swap_erase_trailer_sectors(state, fap_sec);
+        assert(rc == 0);
+    }
+
+    rc = boot_erase_region(fap_pri, new_off, sz);
+    assert(rc == 0);
+
+    rc = boot_copy_region(state, fap_pri, fap_pri, old_off, new_off, sz);
+    assert(rc == 0);
+
+    rc = boot_write_status(state, bs);
+
+    bs->idx++;
+    BOOT_STATUS_ASSERT(rc == 0);
+}
+
+#else
+
+static void
+boot_swap_sectors(int idx, uint32_t sz, struct boot_loader_state *state,
+        struct boot_status *bs, const struct flash_area *fap_pri,
+        const struct flash_area *fap_sec)
+{
+    uint32_t pri_off;
+    uint32_t pri_up_off;
+    uint32_t sec_off;
+    int rc;
+
+    pri_up_off = boot_img_sector_off(state, BOOT_PRIMARY_SLOT, idx);
+    pri_off = boot_img_sector_off(state, BOOT_PRIMARY_SLOT, idx - 1);
+    sec_off = boot_img_sector_off(state, BOOT_SECONDARY_SLOT, idx - 1);
+
+    if (bs->state == BOOT_STATUS_STATE_0) {
+        rc = boot_erase_region(fap_pri, pri_off, sz);
+        assert(rc == 0);
+
+        rc = boot_copy_region(state, fap_sec, fap_pri, sec_off, pri_off, sz);
+        assert(rc == 0);
+
+        rc = boot_write_status(state, bs);
+        bs->state = BOOT_STATUS_STATE_1;
+        BOOT_STATUS_ASSERT(rc == 0);
+    }
+
+    if (bs->state == BOOT_STATUS_STATE_1) {
+        rc = boot_erase_region(fap_sec, sec_off, sz);
+        assert(rc == 0);
+
+        rc = boot_copy_region(state, fap_pri, fap_sec, pri_up_off, sec_off, sz);
+        assert(rc == 0);
+
+        rc = boot_write_status(state, bs);
+        bs->idx++;
+        bs->state = BOOT_STATUS_STATE_0;
+        BOOT_STATUS_ASSERT(rc == 0);
+    }
+}
+
 /*
  * "Moves" the sector located at idx - 1 to idx.
  */
@@ -386,46 +531,7 @@ boot_move_sector_up(int idx, uint32_t sz, struct boot_loader_state *state,
     BOOT_STATUS_ASSERT(rc == 0);
 }
 
-static void
-boot_swap_sectors(int idx, uint32_t sz, struct boot_loader_state *state,
-        struct boot_status *bs, const struct flash_area *fap_pri,
-        const struct flash_area *fap_sec)
-{
-    uint32_t pri_off;
-    uint32_t pri_up_off;
-    uint32_t sec_off;
-    int rc;
-
-    pri_up_off = boot_img_sector_off(state, BOOT_PRIMARY_SLOT, idx);
-    pri_off = boot_img_sector_off(state, BOOT_PRIMARY_SLOT, idx - 1);
-    sec_off = boot_img_sector_off(state, BOOT_SECONDARY_SLOT, idx - 1);
-
-    if (bs->state == BOOT_STATUS_STATE_0) {
-        rc = boot_erase_region(fap_pri, pri_off, sz);
-        assert(rc == 0);
-
-        rc = boot_copy_region(state, fap_sec, fap_pri, sec_off, pri_off, sz);
-        assert(rc == 0);
-
-        rc = boot_write_status(state, bs);
-        bs->state = BOOT_STATUS_STATE_1;
-        BOOT_STATUS_ASSERT(rc == 0);
-    }
-
-    if (bs->state == BOOT_STATUS_STATE_1) {
-        rc = boot_erase_region(fap_sec, sec_off, sz);
-        assert(rc == 0);
-
-        rc = boot_copy_region(state, fap_pri, fap_sec, pri_up_off, sec_off, sz);
-        assert(rc == 0);
-
-        rc = boot_write_status(state, bs);
-        bs->idx++;
-        bs->state = BOOT_STATUS_STATE_0;
-        BOOT_STATUS_ASSERT(rc == 0);
-    }
-}
-
+#endif
 /*
  * When starting a revert the swap status exists in the primary slot, and
  * the status in the secondary slot is erased. To start the swap, the status
@@ -528,8 +634,47 @@ swap_run(struct boot_loader_state *state, struct boot_status *bs,
     rc = flash_area_open(FLASH_AREA_IMAGE_SECONDARY(image_index), &fap_sec);
     assert (rc == 0);
 
+#ifdef MCUBOOT_DELTA_UPGRADE
+    uint8_t source_hash[32] = {0};
+    fih_int fih_rc = FIH_FAILURE;
+  
+    flash_device = DEVICE_DT_GET(FLASH_NODEID);
+	if(!flash_device) {
+        BOOT_LOG_INF("Load flash driver failed!!!\r\n");
+		return;
+	}
+
+    fih_rc = get_source_hash(fap_pri, source_hash);
+#ifdef DELTA_ENABLE_LOG
+    if (fih_rc == FIH_SUCCESS)
+    {
+        printf("\nsource_hash read from slot 1:\r\n");
+        for(int i = 0; i < sizeof(source_hash); i++)
+        {
+            printf("%02X ",source_hash[i]);
+        }
+    }
+#endif
+	rc = delta_read_patch_header(source_hash, &patch_size, &opFlag);
+	if (rc < 0) {
+		printf("ret=%d	read patch file error, exit delta update process!!!\n", rc);
+        flash_area_erase(fap_sec, 0, flash_area_get_size(fap_sec));       //just for test
+		return;
+	}
+    else
+    {
+    #ifdef MCUBOOT_WRITE_STATUS_DYNAMIC
+        move_up_pages = 10 + ((patch_size/2)/PAGE_SIZE);
+    #else
+        move_up_pages = (((patch_size/2)/PAGE_SIZE > 0) ? ((patch_size/2)/PAGE_SIZE) : 1) ;
+    #endif
+        printf("##patch_size = %d\t opFlag = %02X\t move_up_pages=%d\t status_address=0X%lX\r\n", patch_size, opFlag, move_up_pages,status_address);
+    }
+#endif
+
     fixup_revert(state, bs, fap_sec);
 
+#ifndef MCUBOOT_DELTA_UPGRADE
     if (bs->op == BOOT_STATUS_OP_MOVE) {
         idx = last_idx;
         while (idx > 0) {
@@ -551,9 +696,80 @@ swap_run(struct boot_loader_state *state, struct boot_status *bs,
         idx++;
     }
 
+#else
+
+/** move up source image move_up_pages pages */
+     if (bs->op == BOOT_STATUS_OP_MOVE) {
+        idx = last_idx;
+        while (idx > 0) {
+            if (idx <= (last_idx - bs->idx + 1)) {
+                boot_move_sector_up_pages(idx, sector_sz, move_up_pages, state, bs, fap_pri, fap_sec);
+            }
+            idx--;
+        }
+        bs->idx = BOOT_STATUS_IDX_0;
+        bs->op = BOOT_STATUS_OP_APPLY;
+    }
+
+    struct flash_mem flash_pt = {0};
+    /** This step try to find the old image pages which will be used after be erased*/
+    if(opFlag == DELTA_OP_TRAVERSE)
+    { 
+        if(traverse_delta_file(&flash_pt, &apply_patch) > 0)
+        {
+            opFlag = DELTA_OP_APPLY;
+        }  
+        else
+        {
+            bs->op = BOOT_STATUS_OP_RESTORE;
+            goto restore;
+        }     
+    }
+    /** Now we start to apply patch file to create new image*/
+    if(opFlag == DELTA_OP_APPLY)
+    { 
+    #ifdef MCUBOOT_WRITE_STATUS_DYNAMIC
+        status_address = get_status_address();
+    #endif
+    /**just for test*/
+        rc = apply_read_status(&flash_pt); 
+        if (rc < 0) {
+            bs->op = BOOT_STATUS_OP_RESTORE;
+            goto restore;
+        }  
+        
+        printf("##Now start applying patch file!!!\r\n");
+
+        rc = delta_apply_init(&flash_pt,patch_size,&apply_patch);
+        if (rc < 0) {
+            bs->op = BOOT_STATUS_OP_RESTORE;
+            goto restore;
+        }      
+                 
+        delta_check_and_apply(&flash_pt, &apply_patch);
+    }
+
+restore:
+    if(bs->op == BOOT_STATUS_OP_RESTORE)
+    {
+        printf("!!!There is something wrong during apply, let's restore the old image. move_down_pages=%d!!!\r\n", move_up_pages);
+        idx = 1;
+        while (idx <= last_idx) {
+            if (idx >= (bs->idx)) {
+                boot_move_sector_down_pages(idx, sector_sz, move_up_pages, state, bs, fap_pri, fap_sec);
+            }
+            idx++;
+        }
+        bs->idx = BOOT_STATUS_IDX_0;
+        bs->op = BOOT_STATUS_OP_MOVE;
+    }
+#endif
+
     flash_area_close(fap_pri);
     flash_area_close(fap_sec);
 }
+
+
 
 int app_max_size(struct boot_loader_state *state)
 {
